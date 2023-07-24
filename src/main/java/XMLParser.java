3a22cd1584501717d53671d1bfc9526c00b78f57
maddipatiSiriChandana <59562536+maddipatiSiriChandana@users.noreply.github.com>
2023-07-25 01:52:34 +0530

    Update XMLParser.java
    modified code

--- src/main/java/XMLParser.java
+++ src/main/java/XMLParser.java
@@ -28,20 +28,42 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class XMLParser {
-
+	
     public static void main(String[] args) {
         try {
-            // Parse input1.xml and produce output1.txt
-            List<String> output1 = parseInput1("input1.xml");
-            writeOutputToFile(output1, "output1.txt");
+            String inputFileName1 = "input1.xml";
+            String inputFileName2 = "input2.xml";
 
-            // Parse input2.xml and produce output2.txt
-            List<String> output2 = parseInput2("input2.xml");
-            writeOutputToFile(output2, "output2.txt");
+            // Parse input1.xml and input2.xml
+            List<String> output1 = parseInput1(inputFileName1);
+            List<String> output1Instances = parseInput1Instances(inputFileName1);
+            List<String> output2 = parseInput2(inputFileName2);
+            List<String> output2InstancesAndGroups = parseInput2InstancesAndGroups(inputFileName2);
+		
+	    // Sort the individual lists
+	    output1.sort(String::compareTo);
+            output1Instances.sort(String::compareTo);
+            output2.sort(String::compareTo);
+            output2InstancesAndGroups.sort(String::compareTo);
+
+            // Merge the outputs into a single list
+            List<String> combinedOutput1 = new ArrayList<>(output1);
+            combinedOutput1.addAll(output1Instances);
+            List<String> combinedOutput2 = new ArrayList<>(output2);
+            combinedOutput2.addAll(output2InstancesAndGroups);
+	
+	    combinedOutput1.sort(String::compareTo);
+	    combinedOutput2.sort(String::compareTo);
+
+            // Write connector, instance, and group information to output files
+            writeOutputToFile(combinedOutput1, "output1.txt");
+            writeOutputToFile(combinedOutput2, "output2.txt");
+
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
         }
     }
+    
 
     public static List<String> parseInput1(String inputFileName) throws ParserConfigurationException, SAXException, IOException {
         List<String> output = new ArrayList<>();
@@ -84,41 +106,87 @@ public class XMLParser {
                 output.add("connector:fromInstance=" + fromInstance + ", fromField=" + fromField + ", toInstance=" + toInstance + ", toField=" + toField);
             }
         }
-        nodeList = doc.getElementsByTagName("TRANSFORMATION");
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node = nodeList.item(i);
+        NodeList transformationNodeList = doc.getElementsByTagName("TRANSFORMATION");
+        for (int i = 0; i < transformationNodeList.getLength(); i++) {
+            Node node = transformationNodeList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
                 String name = element.getAttribute("NAME");
-                if (name.equals("trans_ROUTER_EMPLOYEES_BY_JOB_ID")) {
-                    NodeList fieldList = element.getElementsByTagName("FIELD");
-                    for (int j = 0; j < fieldList.getLength(); j++) {
-                        Node fieldNode = fieldList.item(j);
+                if (name.equals("trans_ROUTER_EMPLOYEES_BY_JOB_ID") || name.equals("UC05_ROUTER_EMPLOYEES_MGR")) {
+                    NodeList fieldNodeList = element.getElementsByTagName("FIELD");
+                    for (int j = 0; j < fieldNodeList.getLength(); j++) {
+                        Node fieldNode = fieldNodeList.item(j);
                         if (fieldNode.getNodeType() == Node.ELEMENT_NODE) {
                             Element fieldElement = (Element) fieldNode;
-                            String fieldName = fieldElement.getAttribute("NAME");
-                            output.add("connector:fromInstance=SQ_EMPLOYEES, fromField=" + fieldName + ", toInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, toField=" + fieldName);
+                            String fromField = fieldElement.getAttribute("NAME");
+                            String toField = fieldElement.getAttribute("PORTTYPE");
+                            output.add("fromField=" + fromField + ", toField=" + toField);
                         }
                     }
-                } else if (name.equals("UC05_ROUTER_EMPLOYEES_MGR")) {
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=EMPLOYEE_ID1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, toField=EMPLOYEE_ID");
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=FIRST_NAME1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, told=FIRST_NAME");
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=LAST_NAME1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, toField=LAST_NAME");
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=EMAIL1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, toField=EMAIL");
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=PHONE_NUMBER1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, toField=PHONE_NUMBER");
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=HIRE_DATE1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, toField=HIRE_DATE");
-                    output.add("connector:fromInstance=trans_ROUTER_EMPLOYEES_BY_JOB_ID, fromField=JOB_ID1, toInstance=UC05_ROUTER_EMPLOYEES_MGR, toField=JOB_ID");
                 }
             }
         }
         return output;
     }
 
-    public static void writeOutputToFile(List<String> output, String outputFileName) throws IOException, TransformerException {
-        FileWriter writer = new FileWriter(outputFileName);
-        for (String line : output) {
-            writer.write(line + "\n");
+    public static List<String> parseInput1Instances(String inputFileName) throws ParserConfigurationException, SAXException, IOException {
+        List<String> output = new ArrayList<>();
+        File inputFile = new File(inputFileName);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList nodeList = doc.getElementsByTagName("INSTANCE");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String name = element.getAttribute("NAME");
+                String type = element.getAttribute("TYPE");
+                output.add("instance:Name=" + name + ", type=" + type);
+            }
         }
-        writer.close();
+        return output;
     }
-}
+
+    public static List<String> parseInput2InstancesAndGroups(String inputFileName) throws ParserConfigurationException, SAXException, IOException {
+        List<String> output = new ArrayList<>();
+        File inputFile = new File(inputFileName);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList instanceNodeList = doc.getElementsByTagName("INSTANCE");
+        for (int i = 0; i < instanceNodeList.getLength(); i++) {
+            Node node = instanceNodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String name = element.getAttribute("NAME");
+                String type = element.getAttribute("TYPE");
+                output.add("instance:Name=" + name + ", type=" + type);
+            }
+        }
+        NodeList groupNodeList = doc.getElementsByTagName("GROUP");
+        for (int i = 0; i < groupNodeList.getLength(); i++) {
+            Node node = groupNodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String name = element.getAttribute("NAME");
+                String expression = element.getAttribute("EXPRESSION");
+                String order = element.getAttribute("ORDER");
+                String type = element.getAttribute("TYPE");
+                output.add("group:name=" + name + ", expression=" + expression + ", order=" + order + ", type=" + type);
+            }
+        }
+        return output;
+    }
+
+    public static void writeOutputToFile(List<String> output, String outputFileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(outputFileName);
+        for (String line : output) {
+            fileWriter.write(line + "\n");
+        }
+        fileWriter.close();
+    }
+
+    }
